500be313b77c232e5caecbe0ee5a75f00ae196b7
koejuao <santana.br@outlook.com.br>
2019-07-16 14:16:03 -0300

    add/delete(localStorage)

--- todos.js
+++ todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer cafe',
-    'Estudar Javascript',
-    'Acessar comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     listElement.innerHTML = '';
@@ -28,7 +24,7 @@ function renderTodos() {
 
     todoElement.appendChild(todoText);
     todoElement.appendChild(linkElement);
-    
+
     listElement.appendChild(todoElement);
   }
 }
@@ -41,10 +37,19 @@ function addTodo(){
   todos.push(todoText);
   inputElement.value = '';
   renderTodos();
+  saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
 
 function deleteTodo (pos){
   todos.splice(pos, 1);
+  renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage(){
+  
+  localStorage.setItem('list_todos',JSON.stringify(todos));
+
 }
