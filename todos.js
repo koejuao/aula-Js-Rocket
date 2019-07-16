fc0b82d4697b0fa53424b6321c42d0efd0530b19
koejuao <santana.br@outlook.com.br>
2019-07-16 13:40:12 -0300

    js

--- todos.js
+++ todos.js
@@ -3,17 +3,46 @@ var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
 var todos = [
-    'Fazer café'
-    'Estudar Javascript'
+    'Fazer cafe',
+    'Estudar Javascript',
     'Acessar comunidade da Rocketseat'
 ];
 
 function renderTodos() {
+    listElement.innerHTML = '';
+
     for (todo of todos) {
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement('a');
+
+    linkElement.setAttribute('href','#');
+
+    var pos = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo('+ pos +')');
+
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
+
     todoElement.appendChild(todoText);
     listElement.appendChild(todoElement);
   }
+}
+
+renderTodos();
+ 
+function addTodo(){
+  var todoText = inputElement.value;
+
+  todos.push(todoText);
+  inputElement.value = '';
+  renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo (pos){
+  todos.splice(pos, 1);
 }
